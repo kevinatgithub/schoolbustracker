57fe045c2d2a2c3dafedfcf27b14c605a8856b4d
Kevin <kdcainday@nbbnets.net>
2019-03-18 14:55:41 +0700

    Drivers List on going

---
+++ Admin/app/src/main/java/dev/kevin/app/schoolbustrackeradmin/DriversActivity.java
@@ -0,0 +1,18 @@
+package dev.kevin.app.schoolbustrackeradmin;
+
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class DriversActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drivers);
+
+    }
+}
