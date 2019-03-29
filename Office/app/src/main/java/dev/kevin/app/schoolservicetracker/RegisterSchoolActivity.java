cda09bcd074f8fbf459cc216b9c4e732496f1c3d
Kevin <kdcainday@nbbnets.net>
2019-03-29 13:01:55 +0700

    New App For Office

---
+++ Office/app/src/main/java/dev/kevin/app/schoolservicetracker/RegisterSchoolActivity.java
@@ -0,0 +1,13 @@
+package dev.kevin.app.schoolservicetracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterSchoolActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_school);
+    }
+}
